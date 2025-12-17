fd7203cb2954097dda405ec84f3d10fe66311b03
dany <prathapadany@gmail.com>
2025-12-17 11:55:23 +0530

    code

--- Day6/countvowels1.java
+++ Day6/countvowels1.java
@@ -7,7 +7,10 @@ public class countvowels1 {
         int count=0;
         s=s.toLowerCase();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i))
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
+                count++;
+            }
+       System.out.println(count);
         }
     }
     
