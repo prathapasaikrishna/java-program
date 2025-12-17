52d60f183b375346cb686d7b520e3b2406c52713
dany <prathapadany@gmail.com>
2025-12-17 11:32:20 +0530

    i write the code

---
+++ Day6/countvowels.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class countvowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.nextLine();
+        
+
+        String s=sc.nextLine();
+        System.out.println(a+" "+s);
+
+
+    }
+    
+}
