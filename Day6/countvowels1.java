ca7ded5299bb175641578be72518ae663db61d4a
dany <prathapadany@gmail.com>
2025-12-17 11:50:02 +0530

    sucess the program

---
+++ Day6/countvowels1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class countvowels1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int count=0;
+        s=s.toLowerCase();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i))
+        }
+    }
+    
+}
